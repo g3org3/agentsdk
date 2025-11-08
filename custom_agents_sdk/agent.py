4fc44da5abfc998f71d3e5dd7b3b444f63cf3efc
g3org3 <7jagjag@gmail.com>
2025-11-08 18:23:47 +0100

    test module

---
+++ custom_agents_sdk/agent.py
@@ -0,0 +1,3 @@
+class Agent:
+    def test(self):
+        print("test")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+_ = setup(
+    name="custom_agents_sdk",
+    verison="0.0.1",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
